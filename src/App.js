2051f570928f6132ff4d51ae4169544c4390760c
Ted <82258008+wwwourstudio@users.noreply.github.com>
2023-05-24 21:07:47 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import Model from "./Model";
+
+export default function App() {
+  return (
+    <div>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
